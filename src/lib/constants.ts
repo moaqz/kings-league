419218d6e221260eb99e15b14c7761eee1426a87
moaqz <goz.matu@gmail.com>
2023-07-05 17:28:06 -0300

    feat: add header component

---
+++ src/lib/constants.ts
@@ -0,0 +1,100 @@
+export const LINKS = [
+  {
+    href: "https://kingsleague.pro/equipos/",
+    text: "Equipos",
+  },
+  {
+    href: "https://kingsleague.pro/jugador-12/",
+    text: "Jugador 12 y 13",
+  },
+  {
+    href: "https://kingsleague.pro/partidos/",
+    text: "Partidos",
+  },
+  {
+    href: "https://kingsleague.pro/clasificacion/",
+    text: "Clasificación",
+  },
+  {
+    href: "https://kingsleague.pro/estadisticas/",
+    text: "Estadísticas",
+  },
+  {
+    href: "https://kingsleague.pro/presupuestos/",
+    text: "Presupuestos",
+  },
+];
+
+export const TEAMS = [
+  {
+    name: "aniquiladores-fc",
+    logo: "/teams/logos/aniquiladores.svg",
+  },
+  {
+    name: "porcinos-fc",
+    logo: "/teams/logos/porcinos-fc.svg",
+  },
+  {
+    name: "xbuyer-team",
+    logo: "/teams/logos/xbuyer.svg",
+  },
+  {
+    name: "pio-fc",
+    logo: "/teams/logos/pio-fc.svg",
+  },
+  {
+    name: "el-barrio",
+    logo: "/teams/logos/el-barrio.svg",
+  },
+  {
+    name: "1k",
+    logo: "/teams/logos/1k.svg",
+  },
+  {
+    name: "rayo-barcelona",
+    logo: "/teams/logos/rayo-barcelona.svg",
+  },
+  {
+    name: "ultimate-mostoles",
+    logo: "/teams/logos/ultimate-mostoles.svg",
+  },
+  {
+    name: "los-troncos-fc",
+    logo: "/teams/logos/los-troncos.svg",
+  },
+  {
+    name: "kunisports",
+    logo: "/teams/logos/kunisports.svg",
+  },
+  {
+    name: "jijantes",
+    logo: "/teams/logos/jijantes.svg",
+  },
+  {
+    name: "saiyans-fc",
+    logo: "/teams/logos/saiyans-fc.svg",
+  },
+];
+
+export const SOCIAL_NETWORKS = [
+  {
+    name: "Twitter",
+    icon: "/social/twitter.svg",
+    href: "https://twitter.com/kingsleague",
+  },
+  {
+    name: "Tiktok",
+    icon: "/social/tiktok.svg",
+    href: "https://www.tiktok.com/@kingsleague",
+  },
+  {
+    name: "Instagram",
+    icon: "/social/instagram.svg",
+    href: "https://www.instagram.com/kingsleague",
+  },
+  {
+    name: "Youtube",
+    icon: "/social/youtube.svg",
+    href: "https://www.youtube.com/@KingsLeagueOfficial",
+  },
+];
