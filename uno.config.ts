e6a6a3ec2531b144354d5d65cb7cf0dc6c05430c
moaqz <goz.matu@gmail.com>
2023-07-05 21:42:18 -0300

    feat: add font family

--- uno.config.ts
+++ uno.config.ts
@@ -3,4 +3,10 @@ import transformerVariantGroup from "@unocss/transformer-variant-group";
 
 export default defineConfig({
   transformers: [transformerVariantGroup()],
+  theme: {
+    fontFamily: {
+      body: ["Archivo", "system-ui", "sans-serif"],
+      title: ["Archivo Black", "system-ui", "sans-serif"],
+    },
+  },
 });
